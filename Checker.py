4b0555c293675896a3ee33b2e1b69a0b9f7899b0
Juan Gonzalez De Mendoza <jdemendo@cisco.com>
2019-07-31 12:29:12 -0700

    added loading screen

--- Checker.py
+++ Checker.py
@@ -1,96 +1,157 @@
-#from googlesearch import search_news, get_page
-
-#import re
-
-#import articleDateExtractor
-
-#import urllib.request
-
-
-
-
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen, Request
+from datetime import date
+import re
 
 class Article:
+	def __init__(self, title, date, url,warning, words):
+		self.title = title
+		self.date = date
+		self.url = url
+		self.warning = warning
+		self.words = words
+		
+class Message:
+	def __init__(self, author, title, warning, message, words):
+		self.author = author
+		self.title = title
+		self.warning = warning
+		self.message = message
+		self.words = words
 
 
+def get_news(supplier, quantity):
+	"""
+	Retrieves news about the provided supplier by looking at the Google News RSS feed.
 
-	def __init__(self):
+	Parameters:
+	supplier (Supplier): a Supplier class object to search the news for (TODO: change to this, right now string)
+    quantity (int): the number of news articles to return
 
-		self.title = ''
-
-		self.date = ''
-
-		self.url = ''
-
-
-
-def get_news(input, quantity=10):
-
-
+	Returns:
+	list (Article): list of the first 'quantity' number of results
+	"""
+	if quantity > 100 or quantity <= 0:
+		raise Exception(
+			"Quantity should not be greater than 100 or less than or equal to 0. The value of quantity was {}."
+			.format(quantity))
 
 	articles = []
+	#print('searching ', supplier.name)
+	news_url = "https://news.google.com/rss/search?q={}".format(supplier.name)
+	news_url = news_url.replace(' ', '%20')
 
-	
+	client = urlopen(news_url)
+	xml_page = client.read()
+	client.close()
 
+	soup_page = soup(xml_page, "xml")
+	news_list = soup_page.findAll("item")
 
+	# Print news title, url and publish date
+	for count in range(0, quantity):
 
-	# for url in search_news(input, tld="co.in", num=10, stop=quantity, pause=2): 
+		words = []
+		news = news_list[count]
+		warning = 0
+		headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+		reg_url = news.link.text
+		req = Request(url=reg_url, headers=headers) 
+		try:
+			html = urlopen(req).read() 
+		
+			hwarning_words = retrieve_list('h')
+			lwarning_words = retrieve_list('m')
 
-	# 	print(url)
-
-	# 	temp_article = Article()
-
-	# 	date = '0-0-0'
-
-	# 	content = str(get_page(url))
-
-	# 	title = re.search('<title>(.+?)</title>', content)
-
-	# 	print(content)
-
-	# 	if title:
-
-	# 		#print(title)
-
-	# 		#date_time = articleDateExtractor.extractArticlePublishedDate(url) #This is not working
-
-	# 		if True:
-
-	# 			temp_article.title = title
-
-	# 			temp_article.date = date
-
-	# 			articles.append(temp_article)
-
-
-
-	# 		else:
-
-	# 			pass
+			for word in lwarning_words:
+				if word in str(html):
+					words.append(word)
+					
+					warning = 1
+					
+					if(supplier.highest_warning < 1):
+						supplier.highest_warning = 1
 
 
+			for word in hwarning_words:
+				if word in str(html):
+					words.append(word)
+					
+					warning = 2
+					if(supplier.highest_warning < 2):
+						supplier.highest_warning = 2
+			
 
-	# 	else:
-
-	# 		print("ERROR: No title found on website " + url)
-
-	fake_article = Article()
-	fake_article2 = Article()
-
-	
-
-	fake_article.title = 'Heat Pipes Market: Industry Latest Trends & Global Outlook 2016-2024'
-	fake_article.url = 'http://zmrindustryanalysis.com/7971/heat-pipes-market-industry-latest-trends-global-outlook-2016-2024/'
-	
-	articles.append(fake_article)
-	fake_article2.title = "google"
-	fake_article2.url = 'http://google.com'
-	articles.append(fake_article2)
-	
+			articles.append(Article(news.title.text, news.pubDate.text, news.link.text, warning, words))
+		except:
+			pass
 	return articles
 
+def get_cisco_news(supplier):
+	database = open('database.txt', 'r')
+	messages = []
+	line = database.readline()
+	warning_level = {'High':2, 'Medium':1}
+	words = []
+	warning_num = 0
+
+	while(line):
+		if(line.rstrip('\n') == supplier.name):
+			title = database.readline().rstrip('\n')
+			warning = database.readline().rstrip('\n')
+			message = database.readline().rstrip('\n')
+			author = database.readline().rstrip('\n')
+			
+			hwarning_words = retrieve_list('h')
+			lwarning_words = retrieve_list('m')
+
+			for word in lwarning_words:
+				if word in message:
+					words.append(word)
+					
+					warning_num = 1
+					
+					if(supplier.highest_warning < 1):
+						supplier.highest_warning = 1
 
 
-def search_supplier(supplier):
+			for word in hwarning_words:
+				if word in message:
+					words.append(word)
+					
+					warning_num = 2
+					if(supplier.highest_warning < 2):
+						supplier.highest_warning = 2
 
-	supplier.articles = get_news(supplier.name)
+			if(warning != 'No'):
+				if(warning_num < warning_level[warning]):
+					warning_num = warning_level[warning]
+
+			messages.append(Message(author,title,warning_num,message, words))
+		else:
+			database.readline()
+			database.readline()
+			database.readline()
+			database.readline()
+		line = database.readline()
+				
+	supplier.messages = messages
+
+def search_supplier(supplier, quantity=10):
+	
+	articles = get_news(supplier, 3)
+	supplier.articles = articles
+	get_cisco_news(supplier)
+	
+	
+def retrieve_list(level):
+
+	if(level == 'h'):
+		text_file = open('high_warning.txt', 'r')
+	else:
+		text_file = open('med_warning.txt', 'r')
+	words = text_file.read()
+	text_file.close()
+	return words.split(',')
+
+
--- GUI.py
+++ GUI.py
@@ -7,12 +7,15 @@ from tkinter import BOTH, RAISED, X, Y, END
 import Checker as check
 import time
 import webbrowser
+import sender
+
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 class supplier:
 	def __init__(self,name):
 		self.name = name
 		self.articles = []
+		self.messages = []
 		self.Separator = ''
 		self.article_label = ''
 		self.warning_label = ''
@@ -20,7 +23,10 @@ class supplier:
 		self.drop_button = ''
 		self.clicked = False
 		self.index = 0
-		self.links=[]
+		self.links = []
+		self.labels = []
+		self.highest_warning = 0
+		self.user = ''
 
 class checkerGui:
 
@@ -28,7 +34,7 @@ class checkerGui:
 
 
 		self.window = tk.Tk()
-		self.window.title("Checker")
+		self.window.title("SupMon")
 		self.window.geometry("700x800")
 		self.window.resizable(0, 0)
 
@@ -47,49 +53,92 @@ class checkerGui:
 		self.login_button = tk.Button(self.window, width=370, height = 60,command=self.login, image = button_pic, relief = 'flat')
 		self.login_button.place(x=145,y=690)
 		
-
-
 		self.window.mainloop()
 
 	def login(self):
+		self.user = self.login_entry.get()
 		self.background_label.destroy()
 		self.login_entry.destroy()
 		self.pass_entry.destroy()
 		self.login_button.destroy()
-		#self.loading()
+		self.loading()
 		self.tool_screen()
 	def loading(self):
-		gif = tk.PhotoImage(file ='images/arrow2.png')
-		self.label = tk.Label(self.window, image=gif)
-		self.label.place(x=0,y=0, relwidth=1, relheight=1)
-	def tool_screen(self):
+		
+		self.window.config(bg='white')
+		self.cover_pic = tk.PhotoImage(file='images/cover1.png')
+		self.cover_pich = tk.PhotoImage(file='images/coverh.png')
+		self.frames = [tk.PhotoImage(file='images/loading.gif',format = 'gif -index %i' %(i)) for i in range(24)]
+		self.cisco_photo = tk.PhotoImage(file='images/cisco-513.png')
+		self.cisco_logo = tk.Label(self.window, image=self.cisco_photo, bg = 'white')
+		self.label = tk.Label(self.window, image=self.frames[0])
+		self.cover_label = tk.Label(self.window, image=self.cover_pic, bg= 'white')
+		self.cover_label2 = tk.Label(self.window, image=self.cover_pic, bg= 'white')
+		self.cover_label3 = tk.Label(self.window, image=self.cover_pich, bg= 'white')
+		self.cover_label4 = tk.Label(self.window, image=self.cover_pich, bg= 'white')
 
+		self.cover_label4.place(y=530, x=288)
+		self.cover_label3.place(y=393,x=288)
+		self.cover_label2.place(x=418,y=395)
+		self.cover_label.place(x=283,y=395)
+		self.cisco_logo.place(x=250, y=210)
+		self.label.place(x = 290,y = 400)
+		
+		
+		i = 1
+		j = 0
+		while(j < 2):
+			self.label.config(image=self.frames[i])
+			i += 1
+			if(i == 24):
+				i=0
+				j+=1
+			self.window.update()
+			time.sleep(.1)
+
+		self.label.destroy()
+		self.cisco_logo.destroy()
+		self.cover_label.destroy()
+		self.cover_label2.destroy()
+		self.cover_label3.destroy()
+		self.cover_label4.destroy()
+		self.window.update()
+		
+	def tool_screen(self):
 
 		self.suppliers = []
 		self.menu_offset = 30
 		self.scroller_items = []
-
-		self.window.configure(background='#009EDC')
+		self.window.geometry("1000x800")
+		self.window.configure(background='#0c4361')
 
 		self.arrow_photo = tk.PhotoImage(file='./images/arrow2.png')
-		self.gear_photo = tk.PhotoImage(file = './images/gear.png')
+		self.gear_photo = tk.PhotoImage(file = './images/gear2.png')
 		self.plus_photo = tk.PhotoImage(file = './images/add3.png')
-		self.lwarning_photo = tk.PhotoImage(file = './images/low_warning22.png')
-		self.plane_photo = tk.PhotoImage(file = './images/planeb.png')
+		self.lwarning_photo = tk.PhotoImage(file = './images/low_warning3.png')
+		self.hwarning_photo = tk.PhotoImage(file = './images/high_warning.png')
+		self.plane_photo = tk.PhotoImage(file = './images/planec.png')
+		self.refresh_photo = tk.PhotoImage(file = './images/refresh.png')
 
 		self.add_button = tk.Button(self.window, command = self.__pop_up)
-		self.add_button.config(image=self.plus_photo, width = '30', height = '30', relief=tk.FLAT,background='#009EDC')
+		self.add_button.config(image=self.plus_photo, width = '39', height = '38', relief=tk.FLAT,background='#0c4361')
 		self.add_button.grid(row=0,column=0)
 
 		self.settings_button = tk.Button(self.window, command=self.settings)
-		self.settings_button.config(image=self.gear_photo, width = '30', height = '30', relief=tk.FLAT,background='#009EDC')
+		self.settings_button.config(image=self.gear_photo, width = '39', height = '38', relief=tk.FLAT,background='#0c4361')
 		self.settings_button.grid(row=0,column=1)
 		
-		self.database_button = tk.Button(self.window, image=self.plane_photo, width = '30', height = '30', relief=tk.FLAT, background='#009EDC')
+		self.database_button = tk.Button(self.window, image=self.plane_photo, width = '39', height = '38', relief=tk.FLAT, background='#0c4361')
 		self.database_button.grid(row=0,column = 2)
 		self.database_button.config(command=self.add_article)
-		self.line = ttk.Separator(self.window, orient='horizontal')
-		self.line.place(x=0, y=35, relwidth=1)
+
+		self.place_holder = tk.Label(self.window, width = '81', bg='#0c4361')
+		self.place_holder.grid(row=0, column=3)
+		self.refresh_button = tk.Button(self.window, image=self.refresh_photo, width = 39, height = 39, relief = tk.FLAT, background='#0c4361')
+		self.refresh_button.grid(row=0, column=4)
+		self.refresh_button.config(command=self.refresh)
+
+
 	def add_article(self):
 		self.popup_window = tk.Toplevel()
 		self.popup_window.title('Cisco Database')
@@ -111,12 +160,12 @@ class checkerGui:
 		self.warning_option = tk.Label(self.popup_window, text='Warning:', bg='white')
 		self.warning_option.grid(row=0,column=3,padx=5,pady=5)
 
-		levels = ['low', 'medium', 'high']
+		levels = ["No", 'Medium', 'High']
 	
 		self.warning_dropd = ttk.Combobox(self.popup_window, values=levels,width=10,state="readonly")
 		self.warning_dropd.grid(row=0, column=4,padx=5,pady=5)
 
-		self.send_button = tk.Button(self.popup_window, text='Send', bg='white')
+		self.send_button = tk.Button(self.popup_window, text='Send', bg='white', command=self.send_database)
 		self.send_button.grid(row=2, column=4)
 
 		self.supplier_label = tk.Label(self.popup_window, text='Supplier:', bg='white')
@@ -124,13 +173,30 @@ class checkerGui:
 
 		self.supplier_text = tk.Entry(self.popup_window, width='25')
 		self.supplier_text.grid(row=2,column=1)
-		#database = open("database.txt",'a')
+		
+
+	def send_database(self):
+		database = open("database.txt", 'a')
+		supplier = self.supplier_text.get()
+		message = self.mess_entry.get("1.0",END)
+		warning = self.warning_dropd.get()
+		title = self.title_entry.get()
+		author = self.user
+
+		while(message.endswith('\n')):
+			message = message[:-1]
+
+		database.write(supplier + '\n' + title + '\n' + warning + '\n' + message + '\n' + author + '\n')
+
+		database.close()
+		self.popup_window.destroy()
+
 
 	def __slide(self, index):
 
 		temp_supplier = self.suppliers[index]
-		num_articles = len(temp_supplier.articles)
-		padding = 40 * num_articles
+		num_articles = len(temp_supplier.articles) + len(temp_supplier.messages)
+		padding = 44 * num_articles
 
 		if not temp_supplier.clicked:
 
@@ -142,29 +208,37 @@ class checkerGui:
 					
 					temp_supplier2 = self.suppliers[j]
 			 		
-					temp_supplier2.drop_button.place(y = ((j+1) * 50) + i, x = 650)
-					temp_supplier2.warning_label.place(y = ((j+1) * 50)-5 + i, x = 610)
-					temp_supplier2.article_label.place(y = ((j+1) * 50) + i, x = 590)
+					temp_supplier2.drop_button.place(y = ((j+1) * 50) - 8 + i, x = 950)
+					
+					temp_supplier2.article_label.place(y = ((j+1) * 50) + i, x = 890)
 					temp_supplier2.name_label.place(y = ((j+1) * 50)+i, x = 0)
 					temp_supplier2.Separator.place(y= ((j+1) *50)+40 + i, x = 0,relwidth=1)
+
+					if(temp_supplier2.warning_label != ''):
+						temp_supplier2.warning_label.place(y = ((j+1) * 50)-5 + i, x = 910)
+
 				self.window.update()
 				
 				i += 2
 			temp_supplier.clicked = True
 
-			for k in range(0,num_articles):
+			for k in range(0,len(temp_supplier.articles)):
 				
-				url = temp_supplier.articles[k].url
-				self.add_url(k,url,temp_supplier,index)
+				
+				self.add_url(k,temp_supplier,index)
+
+			for k in range(0,len(temp_supplier.messages)):
+				self.add_message(k,temp_supplier,index)
 
 
 		else:
 			i = padding
 			
 			for k in range(0,num_articles):
-				
 				temp_supplier.links[k].destroy()
+				temp_supplier.labels[k].destroy()
 			del temp_supplier.links[:]
+			del temp_supplier.labels[:]
 
 			while (i > 0):
 
@@ -172,41 +246,69 @@ class checkerGui:
 				for j in range(len(self.suppliers)-1, index , -1):
 					
 					temp_supplier2 = self.suppliers[j]
-					temp_supplier2.drop_button.place(y = ((j+1) * 50) + i, x = 650)
-					temp_supplier2.warning_label.place(y = ((j+1) * 50)-5 + i, x = 610)
-					temp_supplier2.article_label.place(y = ((j+1) * 50) + i, x = 590)
+					temp_supplier2.drop_button.place(y = ((j+1) * 50) - 8 + i, x = 950)
+					temp_supplier2.article_label.place(y = ((j+1) * 50) + i, x = 890)
 					temp_supplier2.name_label.place(y = ((j+1) * 50) + i, x = 0)
 					temp_supplier2.Separator.place(y= ((j+1) *50)+40 + i, x = 0,relwidth=1)
+					if(temp_supplier2.warning_label != ''):
+						temp_supplier2.warning_label.place(y = ((j+1) * 50)-5 + i, x = 910)
 				self.window.update()
 
 				i -= 2
 			temp_supplier.clicked = False
-			
+	
+	def hover_on(self,event, placement, words):
+
+		text_displayed = ''
+
+		for word in words:
+			text_displayed = text_displayed + word +', '
+		
+		text_displayed = text_displayed[:-2]
+
+		self.hover_label = tk.Label(self.window, text=text_displayed)
+		self.hover_label.place(x=415, y=placement - 10)
+
+	def hover_off(self,event):
+		self.hover_label.destroy()
 
 	def callback(self,url):
 
 		 webbrowser.open_new(url)
+
+	
 	def settings(self):
 
 		self.popup_window = tk.Toplevel()
 		self.popup_window.title('Settings')
+		self.popup_window.config( bg='white')
 		#self.popup_window.geometry("600x400")
 
-		self.hwarning_label = tk.Label(self.popup_window, text='High Warning Triggers:')
-		self.mwarning_label = tk.Label(self.popup_window, text='Medium Warning Triggers:')
+		self.hwarning_label = tk.Label(self.popup_window, text='High Warning Triggers:', bg='white')
+		self.mwarning_label = tk.Label(self.popup_window, text='Medium Warning Triggers:', bg='white')
 
-		self.hwarning_entries = tk.Text(self.popup_window, width = 25, height = 10)
-		self.mwarning_entries = tk.Text(self.popup_window, width = 25, height = 10)
+		self.hwarning_entries = tk.Text(self.popup_window, width = 30, height = 8)
+		self.mwarning_entries = tk.Text(self.popup_window, width = 30, height = 8)
 
-		self.save_button = tk.Button(self.popup_window, text='Save', command=self.save)
+		self.int_var = tk.IntVar(value=3)
+		self.email_radio = tk.Radiobutton(self.popup_window,text = 'Email', bg='white',variable = self.int_var, value = 3)
+		self.webex_radio = tk.Radiobutton(self.popup_window,text = 'Webex', bg='white', variable = self.int_var, value = 4)
+		self.mobile_radio = tk.Radiobutton(self.popup_window,text = 'Mobile', bg='white',variable = self.int_var, value = 5)
+
+		self.save_button = tk.Button(self.popup_window, text='Save', command=self.save, bg='white')
+		self.notification_label = tk.Label(self.popup_window, text='Notifications', bg='white')
 
 		self.hwarning_label.grid(row=0, column = 0)
-		self.mwarning_label.grid(row=1, column=0)
+		self.mwarning_label.grid(row=1, column=0, padx=5)
 
 		self.hwarning_entries.grid(row=0,column = 1, padx = 10, pady = 10)
 		self.mwarning_entries.grid(row=1,column = 1, padx = 10, pady = 10)
 
-		self.save_button.grid(row=2,column=1)
+		self.notification_label.grid(row=5, column=0)
+		self.email_radio.grid(row=4, column=1)
+		self.webex_radio.grid(row=5, column=1)
+		self.mobile_radio.grid(row=6, column=1)
+		self.save_button.grid(row=7,column=0, columnspan=2, pady=10)
 
 		high_file = open('high_warning.txt', 'r')
 		med_file = open('med_warning.txt','r')
@@ -227,8 +329,8 @@ class checkerGui:
 		high_file = open('high_warning.txt', 'w')
 		med_file = open('med_warning.txt','w')
 
-		hwords = self.hwarning_entries.get("1.0",END)
-		mwords = self.mwarning_entries.get("1.0",END)
+		hwords = self.hwarning_entries.get("1.0",END).rstrip('\n')
+		mwords = self.mwarning_entries.get("1.0",END).rstrip('\n')
 		high_file.write(hwords)
 		med_file.write(mwords)
 
@@ -276,40 +378,109 @@ class checkerGui:
 			pass
 		
 	def add_supplier(self):
-
+		
 		temp_supplier = supplier(self.text)
 		check.search_supplier(temp_supplier)
 		temp_supplier.index = len(self.suppliers)
 		self.suppliers.append(temp_supplier)
 
-		number_of_articles = len(temp_supplier.articles)
-		temp_supplier.name_label = tk.Label(self.window, text=self.text,background='#009EDC',fg='white')
+		number_of_articles = len(temp_supplier.articles) + len(temp_supplier.messages)
+		temp_supplier.name_label = tk.Label(self.window, text=self.text,background='#0c4361',fg='white')
 		temp_supplier.name_label.place(y = (len(self.suppliers) * 50), x = 0)
+		self.window.update()
+		if(temp_supplier.highest_warning == 2):
+			temp_supplier.warning_label = tk.Label(self.window,image = self.hwarning_photo,background='#0c4361')
+			temp_supplier.warning_label.place(y = (len(self.suppliers) * 50)-5, x = 910)
 
-		
-		temp_supplier.warning_label = tk.Label(self.window,image = self.lwarning_photo,background='#009EDC')
-		temp_supplier.warning_label.place(y = (len(self.suppliers) * 50)-5, x = 610)
+		elif(temp_supplier.highest_warning == 1):
+			temp_supplier.warning_label = tk.Label(self.window,image = self.lwarning_photo,background='#0c4361')
+			temp_supplier.warning_label.place(y = (len(self.suppliers) * 50)-5, x = 910)
 
-		temp_supplier.article_label = tk.Label(self.window, text=number_of_articles,background='#009EDC')
-		temp_supplier.article_label.place(y = (len(self.suppliers) * 50), x = 590)
+		temp_supplier.article_label = tk.Label(self.window, text=number_of_articles,background='#0c4361', fg='white')
+		temp_supplier.article_label.place(y = (len(self.suppliers) * 50), x = 890)
 
 		temp_supplier.Separator = ttk.Separator(self.window, orient='horizontal')
 		temp_supplier.Separator.place(y=(len(self.suppliers)*50)+40, x = 0,relwidth=1)
 
 		temp_supplier.drop_button = tk.Button(self.window, command = lambda: self.__slide(temp_supplier.index))
-		temp_supplier.drop_button.config(image=self.arrow_photo, width='39', height='32',relief=tk.FLAT,background='#009EDC')
-		temp_supplier.drop_button.place(y = (len(self.suppliers) * 50), x = 650)
+		temp_supplier.drop_button.config(image=self.arrow_photo, width='43', height='42',relief=tk.FLAT,background='#0c4361')
+		temp_supplier.drop_button.place(y = (len(self.suppliers) * 50) - 8, x = 950)
+		self.window.update()
+		return temp_supplier
 
+	def destroy_all(self):
 
-	def add_url(self,k,url,temp_supplier,index):
+		for supplier in self.suppliers:
+			supplier.drop_button.destroy()
+			supplier.Separator.destroy()
+			supplier.article_label.destroy()
+			if(supplier.highest_warning > 0):
+				supplier.warning_label.destroy()
+			supplier.name_label.destroy()
 
-		temp = tk.Button(self.window, text=temp_supplier.articles[k].title, fg="black", cursor="hand2", relief = tk.FLAT, anchor = 'w')
+	def add_url(self,k,temp_supplier,index):
 
+		url = temp_supplier.articles[k].url
+		display = ''
+		max = 36
+
+		if(len(temp_supplier.articles[k].title) > max):
+			display = temp_supplier.articles[k].title[:max] + '...'
+		else:
+			display = temp_supplier.articles[k].title
+
+		temp = tk.Button(self.window, text=display, fg="black", cursor="hand2", relief = tk.FLAT, anchor = 'w')
 		temp_supplier.links.append(temp)
-	
-		temp.config(command = lambda: self.callback(url))
-		temp.place(y=((index+1)*50)+40 + (k*40), x = 0,relwidth=1)
+		temp.config(command = lambda: self.callback(url), width = 33, height = 1, bg='#0c4361', fg='white', font="Verdana 10 underline")
 
+		war_label = tk.Button(self.window, relief = tk.FLAT, bg='#0c4361')
+		temp_supplier.labels.append(war_label)
+
+		placement = ((index+1)*50)+35 + (k*40)
+		words = temp_supplier.articles[k].words
+		if(temp_supplier.articles[k].warning == 1):
+			war_label.config(image=self.lwarning_photo, width=40, height=40)
+			war_label.place(y=placement, x = 410)
+			war_label.bind('<Enter>', lambda a, b = placement, c = words :self.hover_on(a, b, c))
+			war_label.bind('<Leave>', lambda a: self.hover_off(a))
+
+
+		elif(temp_supplier.articles[k].warning == 2):
+			war_label.config(image=self.hwarning_photo, width=40, height=40)
+			war_label.place(y=placement, x = 410)
+			war_label.bind('<Enter>', lambda a, b = placement, c = words :self.hover_on(a, b, c))
+			war_label.bind('<Leave>', lambda a: self.hover_off(a))
+
+		temp.place(y=((index+1)*50)+40 + (k*40), x = 0)
+
+	def add_message(self,k,temp_supplier,index):
+		message = temp_supplier.messages[k]
+		j = k + len(temp_supplier.articles)
+
+		temp = tk.Button(self.window, text=message.title, fg="black", cursor="hand2", relief = tk.FLAT, anchor = 'w')
+		temp_supplier.links.append(temp)
+		#temp.config(command = lambda: self.open_message(message), width = 33, height = 1, bg='#0c4361', fg='white', font="Verdana 10 underline")
+		temp.config( width = 33, height = 1, bg='#0c4361', fg='white', font="Verdana 10 underline")
+
+		war_label = tk.Button(self.window, relief = tk.FLAT, bg='#0c4361')
+		temp_supplier.labels.append(war_label)
+
+		placement = ((index+1)*50)+35 + (j*40)
+		words = temp_supplier.messages[k].words
+		if(temp_supplier.articles[k].warning == 1):
+			war_label.config(image=self.lwarning_photo, width=40, height=40)
+			war_label.place(y=placement, x = 410)
+			war_label.bind('<Enter>', lambda a, b = placement, c = words :self.hover_on(a, b, c))
+			war_label.bind('<Leave>', lambda a: self.hover_off(a))
+
+
+		elif(temp_supplier.messages[k].warning == 2):
+			war_label.config(image=self.hwarning_photo, width=40, height=40)
+			war_label.place(y=placement, x = 410)
+			war_label.bind('<Enter>', lambda a, b = placement, c = words :self.hover_on(a, b, c))
+			war_label.bind('<Leave>', lambda a: self.hover_off(a))
+
+		temp.place(y=placement, x = 0)
 
 	def browse_button_func(self):
 		suppliers = []
@@ -318,8 +489,26 @@ class checkerGui:
 		with open(self.directory,'r') as file_object:	
 
 			for line in file_object:
-				self.text = line
-				self.add_supplier()
-			
-test = checkerGui()
+				
+				self.text = line.rstrip('\n')
+				supp = self.add_supplier()
+				if(supp.highest_warning == 2):
+					suppliers.append(supp.name)
+		if(suppliers != []):
+			sender.send_email(self.user, suppliers)
 
+	def refresh(self):
+		email_suppliers = []
+		temp = self.suppliers
+		self.destroy_all()
+		self.suppliers = []
+
+		for supplier in temp:
+			self.text = supplier.name
+			supp = self.add_supplier()
+			if(supp.highest_warning == 2):
+				email_suppliers.append(supp.name)
+		if(email_suppliers != []):
+			sender.send_email(self.user, email_suppliers)
+
+test = checkerGui()
